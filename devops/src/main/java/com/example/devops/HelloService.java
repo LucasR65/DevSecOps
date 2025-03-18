fb361a4edf7bbc02a39cc286e0ab010f6f905f69
LucasR65 <158263593+LucasR65@users.noreply.github.com>
2025-03-18 16:58:22 +0100

    update
    ok

--- devops/src/main/java/com/example/devops/HelloService.java
+++ devops/src/main/java/com/example/devops/HelloService.java
@@ -3,8 +3,9 @@ package com.example.devops;
 import org.springframework.web.bind.annotation.*;
 @RestController
 public class HelloService {
-    @GetMapping("/")
-    public String hello() {
-        return "hello";
+    @GetMapping("/bonjour")
+    public Car disBonjour() {
+        Car car = new Car("AA11BB","Ferrari",2000);
+        return car;
     }
 }
